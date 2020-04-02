63ec858c67c52f2281a618ecced5861b99467238
notha99y <renjie.tan91@gmail.com>
2020-04-02 15:51:49 +0800

    make nicer

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import requests
 import shutil
 from tqdm import tqdm
 
+STR_FMT = '%Y-%m-%dT%H:%M:%S'
+
 
 def get_data(time_stamp):
     '''Get LTA realtime traffic data
@@ -20,6 +22,17 @@ def get_data(time_stamp):
 
     return res.json()
 
+def get_camera_info(time_stamp):
+    res = get_data(time_stamp)
+    camera_info = dict()
+    for cam in tqdm(res['items'][0]['cameras']):
+        camera_info[cam['camera_id']] = {
+            'location':cam['location'],
+            'image_metadata':cam['image_metadata']
+        }
+
+    with open('camera_info.json', 'w') as outfile:
+        json.dump(camera_info, outfile)
 
 def save_image_from_url(image_url, image_name):
     resp = requests.get(image_url, stream=True)
@@ -28,19 +41,19 @@ def save_image_from_url(image_url, image_name):
     shutil.copyfileobj(resp.raw, local_file)
     del resp
 
-
-def time_series_images(time_stamp_start, time_stamp_end, camera_id, refresh_rate=60):
-    '''Script to saves images to of a given camera id
-    '''
-    STR_FMT = '%Y-%m-%dT%H:%M:%S'
-    start = datetime.strptime(time_stamp_start, STR_FMT)
-    end = datetime.strptime(time_stamp_end, STR_FMT)
-
-    def datetime_range(start, end, delta):
+def datetime_range(start, end, delta):
         current = start
         while current < end:
             yield current
             current += delta
+            
+def time_series_images(time_stamp_start, time_stamp_end, camera_id, refresh_rate=60):
+    '''Script to saves images to of a given camera id
+    '''
+    start = datetime.strptime(time_stamp_start, STR_FMT)
+    end = datetime.strptime(time_stamp_end, STR_FMT)
+
+    
 
     dts = [dt.strftime(STR_FMT) for dt in datetime_range(
         start, end, timedelta(seconds=refresh_rate))]
@@ -54,18 +67,6 @@ def time_series_images(time_stamp_start, time_stamp_end, camera_id, refresh_rate
         save_image_from_url(
             interested_jui['image'], '{}_{}.jpg'.format(camera_id, time_stamp))
 
-def get_camera_info(time_stamp):
-    res = get_data(time_stamp)
-    camera_info = dict()
-    for cam in tqdm(res['items'][0]['cameras']):
-        camera_info[cam['camera_id']] = {
-            'location':cam['location'],
-            'image_metadata':cam['image_metadata']
-        }
-
-    with open('camera_info.json', 'w') as outfile:
-        json.dump(camera_info, outfile)
-    
 if __name__ == "__main__":
     start_time_stamp = '2020-03-06T10:40:00'
     end_time_stamp = '2020-03-06T11:10:00'
